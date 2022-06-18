2fbb1bdb10c164d30869b47450aff379bf58b1b7
loretta-dhahabu <loreydjefwa@gmail.com>
2022-06-18 19:10:26 +0300

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,77 @@
  for you to use if you need it!
  */
 
+function createEmployeeRecord ( [ firstNm, familyNm, title, payRatePerHour ] )
+{
+   return {
+     firstName: firstNm,
+     familyName: familyNm,
+     title: title,
+     payPerHour: payRatePerHour,
+     timeInEvents: [],
+     timeOutEvents: [],
+   };
+ }
+
+function createEmployeeRecords ( employeeRecArray )
+{
+   let employeeRecords = [];
+   for (let employeeRec of employeeRecArray) {
+     employeeRecords.push(createEmployeeRecord(employeeRec));
+   }
+   return employeeRecords;
+ }
+
+ function createTimeInEvent(dateStamp) {
+   const date = dateStamp.slice(0, 10);
+   const hour = parseInt(dateStamp.slice(11));
+   const newTimeIn = {
+     type: "TimeIn",
+     hour: hour,
+     date: date,
+   };
+   this.timeInEvents.push(newTimeIn);
+   return this;
+ }
+
+function createTimeOutEvent ( dateStamp )
+{
+   const date = dateStamp.slice(0, 10);
+   const hour = parseInt(dateStamp.slice(11));
+   const newTimeOut = {
+     type: "TimeOut",
+     hour: hour,
+     date: date,
+   };
+   this.timeOutEvents.push(newTimeOut);
+   return this;
+ }
+
+ function hoursWorkedOnDate(dateStamp) {
+   let hoursWorked;
+   let hourIn;
+   let hourOut;
+   this.timeInEvents.forEach((timeIn) => {
+     if (timeIn.date === dateStamp) {
+       hourIn = timeIn.hour / 100;
+     }
+   });
+   this.timeOutEvents.forEach((timeOut) => {
+     if (timeOut.date === dateStamp) {
+       hourOut = timeOut.hour / 100;
+     }
+   });
+   hoursWorked = hourOut - hourIn;
+   return hoursWorked;
+ }
+
+ function wagesEarnedOnDate(dateStamp) {
+   const hoursWorked = hoursWorkedOnDate.call(this, dateStamp);
+   const wagesEarned = hoursWorked * this.payPerHour;
+   return wagesEarned;
+ }
+
+
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -20,4 +91,25 @@ const allWagesFor = function () {
 
     return payable
 }
+
+function findEmployeeByFirstName ( srcArray, firstName )
+{
+  let result;
+  srcArray.forEach((src) => {
+    if (src["firstName"] === firstName) {
+      result = src;
+    }
+  });
+  return result;
+}
+
+function calculatePayroll ( employeeRecArray )
+{
+  let totalPayOwed = 0;
+  employeeRecArray.forEach((employeeRec) => {
+    totalPayOwed += allWagesFor.call(employeeRec);
+  });
+  return totalPayOwed;
+}
+
 
